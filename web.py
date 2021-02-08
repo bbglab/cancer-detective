92aa0c031866798499787f8c0c6bfc5c92c40522
Lenka Segura <lenka.segura@irbbarcelona.org>
2021-02-08 18:08:45 +0100

    Modify the template to fit the new results.tsv

--- web.py
+++ web.py
@@ -11,28 +11,26 @@ _THIS_FOLDER = path.dirname(path.abspath(__file__))
 
 
 class Game:
-
-    def __init__(self, mount_point='/'):
+    def __init__(self, mount_point="/"):
         self._mount_point = mount_point
-        self._conf = {
-            '/': {
-            }
-        }
+        self._conf = {"/": {}}
 
         # static dirs for the server
-        static_dirs = ['css', 'images', 'js', 'libs']
-        static_base_dir = path.abspath(path.join(_THIS_FOLDER, 'static'))
+        static_dirs = ["css", "images", "js", "libs"]
+        static_base_dir = path.abspath(path.join(_THIS_FOLDER, "static"))
         for dir in static_dirs:
-            self._conf['/' + dir] = {
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': path.join(static_base_dir, dir)
+            self._conf["/" + dir] = {
+                "tools.staticdir.on": True,
+                "tools.staticdir.dir": path.join(static_base_dir, dir),
             }
 
         # template engine
-        env = Environment(loader=FileSystemLoader(path.join(_THIS_FOLDER, 'templates')),
-                          trim_blocks=True,
-                          lstrip_blocks=True)
-        env.globals['path_info'] = lambda: cherrypy.request.path_info
+        env = Environment(
+            loader=FileSystemLoader(path.join(_THIS_FOLDER, "templates")),
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+        env.globals["path_info"] = lambda: cherrypy.request.path_info
         self._env = env
 
     @property
@@ -45,7 +43,7 @@ class Game:
 
     @cherrypy.expose
     def index(self):
-        raise cherrypy.HTTPRedirect('home')
+        raise cherrypy.HTTPRedirect("home")
 
     @cherrypy.expose
     def home(self):
@@ -63,48 +61,50 @@ class Game:
     def submit(self, **kwargs):
 
         # validate input
-        ttype = kwargs['ttype']
-        if ttype not in {'skin', 'lung'}:
-            raise cherrypy.HTTPError(422, f'Invalid ttype {ttype}')
+        ttype = kwargs["ttype"]
+        if ttype not in {"skin", "lung"}:
+            raise cherrypy.HTTPError(422, f"Invalid ttype {ttype}")
 
         mutations = 0
-        for key in ['riskFactor1', 'riskFactor2', 'riskFactor3']:
+        for key in ["riskFactor1", "riskFactor2", "riskFactor3"]:
             if key not in kwargs:
-                raise cherrypy.HTTPError(422, f'Missing {key}')
+                raise cherrypy.HTTPError(422, f"Missing {key}")
             else:
                 risk_factor = kwargs[key]
-                if risk_factor not in {'1', '2', '3'}:
-                    raise cherrypy.HTTPError(422, f'Invalid risk factor {risk_factor}')
+                if risk_factor not in {"1", "2", "3"}:
+                    raise cherrypy.HTTPError(422, f"Invalid risk factor {risk_factor}")
                 else:
                     mutations += int(risk_factor)
 
-        code = kwargs.get('code', None)
+        code = kwargs.get("code", None)
         if code is not None and not code.isalnum():
-            raise cherrypy.HTTPError(422, f'Invalid code {code}')
+            raise cherrypy.HTTPError(422, f"Invalid code {code}")
 
         # TODO pass a code to mutations so that the cache works
-        return self._env.get_template("results.html").render(mutations=muts.get(ttype, mutations, code=code))
+        return self._env.get_template("results.html").render(
+            mutations=muts.run(ttype, mutations, code=code)
+        )
 
 
 def start_server(conf_file=None):
 
     if conf_file is None:
-        conf_file = path.join(_THIS_FOLDER, 'conf', 'game.cfg')
+        conf_file = path.join(_THIS_FOLDER, "conf", "game.cfg")
         conf_file = conf_file if path.exists(conf_file) else None
 
-    conf_spec = ConfigObj(path.join(_THIS_FOLDER, 'conf', 'game.spec.cfg'))
+    conf_spec = ConfigObj(path.join(_THIS_FOLDER, "conf", "game.spec.cfg"))
     conf = ConfigObj(infile=conf_file, configspec=conf_spec)
     conf.validate(Validator())
 
     # configure the server
-    conf_server = conf['server']
-    cherrypy.config.update({'server.socket_port': int(conf_server['port'])})
-    cherrypy.config.update({'server.socket_host': conf_server['host']})
-    cherrypy.config.update({'environment': conf_server['env']})
+    conf_server = conf["server"]
+    cherrypy.config.update({"server.socket_port": int(conf_server["port"])})
+    cherrypy.config.update({"server.socket_host": conf_server["host"]})
+    cherrypy.config.update({"environment": conf_server["env"]})
 
     # load the app
-    conf_app = conf['app']
-    app = Game(conf_app['mount_point'])
+    conf_app = conf["app"]
+    app = Game(conf_app["mount_point"])
     cherrypy.tree.mount(app, app.mount_point, app.conf)
 
     # start the server
@@ -112,8 +112,9 @@ def start_server(conf_file=None):
     cherrypy.engine.block()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) > 1:
         start_server(sys.argv[1])
     else:
